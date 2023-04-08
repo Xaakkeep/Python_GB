0e96e36671189a54c3d5b68a74b8697610e594b5
Xaakkeep <xaakkeep@gmail.com>
2023-04-08 13:48:08 +0300

    add git

---
+++ lesson 01/main.py
@@ -0,0 +1,16 @@
+import task_02
+import task_04
+import task_06
+import task_08
+
+def main():
+    task_input = int(input('Выберите задние 2, 4, 6, 8: '))
+    if task_input == 2:
+        task_02.task()
+    elif task_input == 4:
+        task_04.task()
+        
+
+
+if __name__ in "__main__":
+    main()
