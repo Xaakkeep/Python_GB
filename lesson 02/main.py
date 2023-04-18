ad9c2609def86e16ea4d91d461cbe732f0a9e59a
Xaakkeep <xaakkeep@gmail.com>
2023-04-18 12:01:33 +0300

    add homework lesson 02

---
+++ lesson 02/main.py
@@ -0,0 +1,18 @@
+import task_10
+import task_12
+import task_14
+
+
+def main():
+    task_input = int(input('Выберите задние 10, 12, 14: '))
+    if task_input == 10:
+        task_10.task()
+    elif task_input == 12:
+        task_12.task()
+    elif task_input == 14:
+        task_14.task()
+      
+
+
+if __name__ in "__main__":
+    main()
